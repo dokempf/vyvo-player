bd43d5c305d8e19f2623b003803f031cad75bb19
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2020-01-05 22:07:18 +0100

    Successful playback with polling

--- mopidy_rfid/frontend.py
+++ mopidy_rfid/frontend.py
@@ -1,4 +1,5 @@
 import pykka
+import time
 
 from mopidy_rfid.devices import select_device
 
@@ -7,5 +8,33 @@ class RFIDFrontend(pykka.ThreadingActor):
     def __init__(self, config, core):
         super(RFIDFrontend, self).__init__()
         self.core = core
+        self.poller = RFIDPollingActor.start(self.actor_ref, config)
 
+    def on_start(self):
+        self.poller.tell(None)
+
+    def on_stop(self):
+        self.poller.stop()
+
+    def on_receive(self, uri):
+        self.core.tracklist.clear()
+        self.core.tracklist.add(uris=[uri])
+        self.core.playback.play()
+
+
+class RFIDPollingActor(pykka.ThreadingActor):
+    def __init__(self, parent, config):
+        super(RFIDPollingActor, self).__init__()
+        self.parent = parent
         self.device = select_device(config)
+        self.current_uri = None
+
+    def on_receive(self, message):
+        uri = self.device.read()
+
+        if uri != self.current_uri:
+            self.current_uri = uri
+            self.parent.tell(uri)
+
+        time.sleep(1)
+        self.actor_ref.tell(None)
