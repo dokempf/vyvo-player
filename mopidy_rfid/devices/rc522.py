f64abcd4bdf8ae0a28d469c04735823018d147dc
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2020-01-08 22:41:18 +0100

    Make it more clear that we reserve block_address 1

--- mopidy_rfid/devices/rc522.py
+++ mopidy_rfid/devices/rc522.py
@@ -3,11 +3,16 @@ from mopidy_rfid.devices import RFIDDeviceBase, MopidyRFIDError
 from pirc522 import RFID
 
 
-def mifare1k_block_address_generator():
+def mifare1k_block_address_generator(reserved_addresses=()):
+    # Yields consecutive 'block addresses' in the pirc522 sense.
+    # S0B0 is always omitted, as well as any block_addresses in
+    # the reserved_addresses list.
+    reserved_addresses = (0,) + tuple(reserved_addresses)
     for sector in range(16):
         for block in range(3):
-            if not ((sector == 0) and (block in (0, 1))):
-                yield sector * 4 + block
+            block_address = sector * 4 + block
+            if block_address not in reserved_addresses:
+                yield block_address
 
 
 def err_handle(ret):
@@ -58,7 +63,10 @@ class RC522Device(RFIDDeviceBase):
 
         # Read all data blocks from the card
         data = []
-        for i, addr in zip(range(0, size, 16), mifare1k_block_address_generator()):
+        for i, addr in zip(
+            range(0, size, 16),
+            mifare1k_block_address_generator(reserved_addresses=(1,)),
+        ):
             data.extend(read_block(addr))
 
         # Truncate padding from the data blocks
@@ -73,8 +81,7 @@ class RC522Device(RFIDDeviceBase):
 
         # If we did not encounter a tag, do nothing
         if tag_type is None:
-            print("Tried writing, but no RFID card found (warning)")
-            return None
+            raise MopidyRFIDError("Tried writing, but no RFID card was found!")
 
         uid = err_handle(self.reader.anticoll())
         err_handle(self.reader.select_tag(uid))
@@ -93,10 +100,19 @@ class RC522Device(RFIDDeviceBase):
         data = bytes(uri, encoding="utf-8")
         size = len(data)
 
+        # Check for the size of the URI and compare against the MIFARE1k data limit
+        # Note that this is the 'lazy' limit which does not go through the hassle
+        # of leveraging available data bytes in sector trailers.
+        if size > 751:
+            raise MopidyRFIDError("URI length exceeds the RFID limit of 751 bytes!")
+
         # Pad data to a multiple of 16
         data = b"".join([data] + [b"0"] * (16 - size % 16))
 
         # Write all data blocks to the card
         write_block(1, size.to_bytes(16, byteorder="little"))
-        for i, addr in zip(range(0, size, 16), mifare1k_block_address_generator()):
+        for i, addr in zip(
+            range(0, size, 16),
+            mifare1k_block_address_generator(reserved_addresses=(1,)),
+        ):
             write_block(addr, data[i : i + 16])
