5de4187f7586615eb965b7110c8b5b19cc8b7877
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2020-12-19 21:29:13 +0100

    First snippets

--- {{cookiecutter._installer}}/install.sh
+++ {{cookiecutter._installer}}/install.sh
@@ -1,7 +1,16 @@
+{# This script is a mixture of regular bash with the Jinja2 templating language! -#}
 #!/bin/bash
 
-# Abort on the first failure
+# Abort this script on the first failure
 set -e
 
-echo "Let's go!"
+# Add the apt.mopidy.com repository
+wget -q -O - https://apt.mopidy.com/mopidy.gpg | apt-key add -
+wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/buster.list
 
+# Update the software present in the downloaded image
+apt update
+apt upgrade
+
+# Install Mopidy including all its necessary GStreamer dependencies
+apt install mopidy
