36d3c0ced4bdc943d38df413d3b7faaa384e6287
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2020-12-20 21:16:24 +0100

    A bit of work on the actual install script

--- {{cookiecutter._installer}}/install.sh
+++ {{cookiecutter._installer}}/install.sh
@@ -14,3 +14,21 @@ apt upgrade
 
 # Install Mopidy including all its necessary GStreamer dependencies
 apt install mopidy
+
+{% if cookiecutter.device == "rc522" %}
+# Enable SPI
+sed -i 's/#dtparam=spi=on/dtparam=spi=on/g' /boot/config.txt
+{% endif %}
+
+{% if cookiecutter.development_setup == "Yes" %}
+# Install some development tools that I typically enjpy having around
+python3 -m pip install IPython pudb
+{% endif %}
+
+# Install the Vyvo Python package
+{% if cookiecutter.development_setup == "Yes" %}
+git clone https://github.com/dokempf/vyvo-player.git
+python3 -m pip install -e vyvo-player
+{% else %}
+python3 -m pip install git+https://github.com/dokempf/vyvo-player.git
+{% endif %}
